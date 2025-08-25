0291b799023b42e4d8530298610312fc6544b1c9
Brad Krajina <bradkrajina@gmail.com>
2025-08-25 09:50:07 -0700

    removing unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import ThreejsSphere from './components/threejs_test'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className = "w-screen h-screen border-red-500">
-        <ThreejsSphere/>
+      <div className="w-screen h-screen border-red-500">
+        <ThreejsSphere />
       </div>
 
     </>
